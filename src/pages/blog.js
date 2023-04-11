d15bf6094d1f691770cb09c294511e5d609e25e4
Anshuman Sinha <anshumansinh@gmail.com>
2023-04-11 03:04:11 -0400

    feat: Generated schema proprely and installed graphQL and GraphQL Yoga lib.

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -12,15 +12,16 @@ In order to connect this Blog to a backend database I need to do the following:
 - Create a SQL table in there to hold the data I need for each blog post. --> COMPLETED
 - Additional Tables? --> COMPLETED
 - Create DEMO/Dummy data in these tables to test on. --> COMPLETED
-- Once Database is created, need to somehow be able to access it. --> THINKING
-- First need to connect it with an ORM? Going to use Prisma.
-- Set up the Prisma Schema.
-- Possibly set up a Prisma client to nteract with the database?
-- Connect it through a local .env file.
-- Once it's connected, we then need to be able to communicate between the frontend and the database.
-- So to query the database, I am going to use GraphQL API
-- To set up the GraphQL Middleware, need to create GraphQL resolvers that use the Prisma client?
-    - Honestly this is a bit confusing but this is my best interpretation so far. Will learn more by doing it I suppose :)
+- Once Database is created, need to somehow be able to access it. --> COMPLETED
+- First need to connect it with an ORM? Going to use Prisma. --> COMPLETED
+- Set up the Prisma Schema. --> COMPLETED
+- Possibly set up a Prisma client to interact with the database? --> COMPLETED
+- Connect it through a local .env file. --> CONNECTED
+- Once it's connected, we then need to be able to communicate between the frontend and the database. --> NOTHING TODO HERE
+- So to query the database, I am going to use GraphQL API --> NOTHING TODO HERE
+- To set up the GraphQL Middleware, need to create GraphQL resolvers that use the Prisma client? --> WORKING
+- Install either the ApolloServer or Yoga libraries to work with GraphQL. I think I will go with Yoga bc it's more lightweight.
+    - Honestly this is a bit confusing but this is my best interpretation so far. Will learn more by doing it I suppose :) --> NOTHING TODO HERE
 - Now I can end this by setting up the GraphQL routes to fetch from the database. Can do this through Next.js qith API routes or SSR
     - I think I will try with API routes first bc from what I understand they're more optimized.
 
