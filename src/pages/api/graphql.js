46f5bc610754e4f8ead5dfc5539b57f55031c5dc
Anshuman Sinha <anshumansinh@gmail.com>
2023-04-12 02:19:31 -0400

    feat: removed Prisma schema and connections, will revist as an extension if desired but for now will focus on GraphQL API development first.

---
+++ src/pages/api/graphql.js
@@ -0,0 +1,14 @@
+import { createYoga, createSchema } from 'graphql-yoga'
+
+export const schema = createSchema({
+    typeDefs: /* GraphQL */ `
+      type Query {
+        hello: String
+      }
+    `,
+    resolvers: {
+      Query: {
+        hello: () => 'world'
+      }
+    }
+  })
