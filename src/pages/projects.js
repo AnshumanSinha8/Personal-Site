58e35c455d27b6a5059e6749f90830e0207b396f
Anshuman Sinha <anshumansinh@gmail.com>
2023-04-15 16:16:25 -0400

    feat: Created propdrilling in Projects component to show temporary array of projects before linking to database.

--- src/pages/projects.js
+++ src/pages/projects.js
@@ -1,15 +1,26 @@
 import Header from '../components/Header.js'
 import Navigation from '../components/Navigation.js';
-import ProjectsComponent from '../components/ProjectsComponent/ProjectsContainer.js';
+import ProjectsContainer from '../components/ProjectsComponent/ProjectsContainer.js';
+import styles from '../styles/ProjectsStyles/Projects.module.css';
 
-
+const projects = [
+    {title: 'Jester', excerpt: 'A low code Jest testing suite generator!'},
+    {title: 'Snake 3000', excerpt: 'An early project to build a retro-styled snake game'}
+]
 
 export default function Projects() {
     return (
-        <div>
+        <div className={styles.projectsPage}>
             <Header />
             <Navigation />
-            <ProjectsComponent />
+            <div>
+                {projects.map((project) => ( <ProjectsContainer project={project} key={project.title}/>
+                    ))
+                }
+            </div>
+            <div>
+                temp
+            </div>
         </div>
     )
 }
