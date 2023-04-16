b6223d2dabac654d42a4abedf214cade933f7f42
Anshuman Sinha <anshumansinh@gmail.com>
2023-04-16 01:40:48 -0400

    feat: Created FE Contact Box.

--- src/components/HomeComponents/Contact.js
+++ src/components/HomeComponents/Contact.js
@@ -4,7 +4,30 @@ import { FaGithub, FaLinkedin } from 'react-icons/fa';
 function Contact(){
     return(
         <div className={styles.contact}>
-            <div className={styles.form}>Contact Me!</div>
+            <div>
+        <form className={styles.form}>
+            <h5>Contact Me!</h5>
+            <label>
+                <input type="text" name="firstName" placeholder="First Name"/>
+                </label>
+                <br />
+            <label>
+                <input type="text" name="lastName" placeholder="Last Name"/>
+                </label>
+                <br />
+            <label>
+                <input type="email" name="email" placeholder="Email"/>
+                </label>
+                <br />
+            <label>
+            
+                <textarea name="message" placeholder="Message" rows="6" cols="40" maxLength="600"></textarea>
+                </label>
+                <br />
+            <button type="submit">Submit</button>
+        </form>
+        </div>
+
             <div className={styles.profileWrapper}>
                 <a href="https://github.com/AnshumanSinha8" target="_blank">
                     <div className={styles.profile}>
@@ -16,10 +39,10 @@ function Contact(){
                     <div className={styles.profile}>
                         <FaLinkedin/>
                         Linkedin
-                    </div>
+                        </div>
                 </a>
-            </div>
-        </div>
+                </div>
+                </div>
     )
 }
 
