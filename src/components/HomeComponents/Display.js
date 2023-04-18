9350d7127d37f8e81459fee027fa253b2ee4d2b9
Anshuman Sinha <anshumansinh@gmail.com>
2023-04-18 04:38:14 -0400

    feat: Huge task out of the way. Successfully rendering content from the database on the client side! Obviously I don't need to be rendering this in the hoe component, but was just using it for development; will fixa nd move to blog page.

--- src/components/HomeComponents/Display.js
+++ src/components/HomeComponents/Display.js
@@ -1,51 +1,41 @@
 import { useQuery, gql } from '@apollo/client';
-import { React } from 'react';
-
-
+import React from 'react';
 
 const QUERY_ALL_POSTS = gql`
-    query GetTestPosts {
-        getTestPosts {
-            id
-            title
-        }
+  query GetTestPosts {
+    getTestPosts {
+      id
+      title
+      content
     }
-`
+  }
+`;
+
 function Display() {
-    // function DisplayPosts() {
-    const { data, loading, error } = useQuery(QUERY_ALL_POSTS)
+  const { data, loading, error } = useQuery(QUERY_ALL_POSTS);
 
-    if (loading) {
-        return <h1> Loading! </h1>
-    }
+  if (loading) {
+    return <h1> Loading! </h1>;
+  }
 
-    if (error) {
-        console.log(error)
-    }
+  if (error) {
+    console.log(error);
+  }
 
-    if (data){
-        console.log(data)
-    }
+  if (data) {
+    console.log(data);
+  }
 
-    return (
-        <div>
-            Posts: 
-        </div>
-    )
-    // return (
-    //     <div>
-    //     {data &&
-    //         data.posts.map((post) => {
-    //         return (
-    //             <div>
-    //                 <h1>Post: {post.id} </h1>
-    //             </div>
-    //         );
-    //         })
-    //     }
-    //     </div>
-    // );
-    // }
+  return (
+    <div>
+      {data &&
+        data.getTestPosts.map((post) => (
+          <div key={post.id}>
+            <h1> Post: {post.id} </h1>
+          </div>
+        ))}
+    </div>
+  );
 }
 
 export default Display;
