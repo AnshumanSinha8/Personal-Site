79f64ee82c2793f3343f6a61e0d48710dafba404
Anshuman Sinha <anshumansinh@gmail.com>
2023-04-18 02:12:34 -0400

    feat: Successfully retrieving all the posts and logging them to the console when querying the Planetscale DB.

--- server/server.js
+++ server/server.js
@@ -1,13 +1,31 @@
-const { ApolloServer } = require ("apollo-server")
-const { typeDefs } = require ("./graphql/typeDefs.js")
+const { ApolloServer } = require ("apollo-server");
+const { typeDefs } = require ("./graphql/typeDefs.js");
 const { resolvers } = require("./graphql/resolvers");
+const { PrismaClient } = require('@prisma/client');
 
+const prisma = new PrismaClient();
+
+// Test function to log all posts in the testPosts schema:
+async function main() {
+  const allPosts = await prisma.testPosts.findMany();
+  console.log(allPosts);
+};
+
+main()
+  .then(async () => {
+    await prisma.$disconnect()
+  })
+  .catch(async (e) => {
+    console.error(e)
+    await prisma.$disconnect()
+    process.exit(1)
+  })
 
 const server = new ApolloServer({
   typeDefs,
   resolvers,
-})
+});
 
 server.listen().then(( {url} ) => {
   console.log(`server is listening on Port: ${url}!`)
-})
+});
