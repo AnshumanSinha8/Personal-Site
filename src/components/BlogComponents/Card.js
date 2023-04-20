34859b2929102e4395be17a8e389ec99e97cb63b
Anshuman Sinha <anshumansinh@gmail.com>
2023-04-20 11:05:16 -0400

    chore: restructure Card and parent component to reduce async function calls to retrieve data.

--- src/components/BlogComponents/Card.js
+++ src/components/BlogComponents/Card.js
@@ -1,33 +1,8 @@
 import styles from 'src/styles/BlogStyles/Card.module.css';
-import { useQuery, gql } from '@apollo/client';
 import Link from 'next/link'
 import React from 'react';
 
-const QUERY_ALL_POSTS = gql`
-  query GetTestPosts {
-    getTestPosts {
-      id
-      title
-      content
-    }
-  }
-`;
-
 function Card({ post }){
-    const { data, loading, error } = useQuery(QUERY_ALL_POSTS);
-
-    if (loading) {
-        return <h6> Loading! </h6>;
-    }
-
-    if (error) {
-        console.log(error);
-    }
-
-    if (data) {
-        console.log(data);
-    }
-
     return(
         <div className={styles.card}>
             <h6> {post.id}. {post.title}{' '}</h6>
