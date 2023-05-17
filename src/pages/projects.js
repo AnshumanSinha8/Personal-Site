f11fd9ebcb5f8e67d299136b5fd028704e538542
Anshuman Sinha <anshumansinh@gmail.com>
2023-05-17 14:16:15 -0500

    fix: Syntax fix

--- src/pages/projects.js
+++ src/pages/projects.js
@@ -26,9 +26,6 @@ export default function Projects() {
                     ))
                 }
             </div>
-            <div>
-                temp
-            </div>
         </div>
     )
 }
