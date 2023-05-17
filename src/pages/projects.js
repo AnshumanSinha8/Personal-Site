6c33c430f7ffdb34288d48a8fda39191aabb3c76
Anshuman Sinha <anshumansinh@gmail.com>
2023-05-17 13:56:37 -0500

    feat: Added more data key:values to projects object

--- src/pages/projects.js
+++ src/pages/projects.js
@@ -4,8 +4,13 @@ import ProjectsContainer from '../components/ProjectsComponent/ProjectsContainer
 import styles from '../styles/ProjectsStyles/Projects.module.css';
 
 const projects = [
-    {title: 'Jester: ', excerpt: 'A low code Jest testing suite generator!'},
-    {title: 'Snake 3000: ', excerpt: 'An early project to build a retro-styled snake game'}
+    {title: 'Jester: ', excerpt: 'A low code Jest testing suite generator'},
+    {title: 'BookBeats: ', excerpt: 'An application to match your current book with a stylized playlist'},
+    {title: 'WispN: ', excerpt: 'Let us Wisp you away to your next National Park journey!'},
+    {title: 'PTC: ', excerpt: 'Having trouble scheduling those pesky Parent Teacher Conferences?'},
+    {title: 'Snake 3000: ', excerpt: 'An early project to build a retro-styled snake game'},
+    {title: 'Railbird: ', excerpt: 'A Freelanced Dashboard Template for Railbird, a sports contract market'}
+
 ]
 const headerContent = {
     title: 'Welcome to my projects!', excerpt: `Take a look around and check out a few projects I'm particularly proud of!`
