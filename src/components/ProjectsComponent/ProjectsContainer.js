a5a00f590d9bba4b8d64ebe234ff28c4c61a1a49
Anshuman Sinha <anshumansinh@gmail.com>
2023-05-21 16:08:46 -0500

    feat: Added Links to project array

--- src/components/ProjectsComponent/ProjectsContainer.js
+++ src/components/ProjectsComponent/ProjectsContainer.js
@@ -5,6 +5,8 @@ import React from 'react';
 function ProjectsContainer({ project }) {
     return (
       <div>
+        <a href={project.link} target ="_blank" rel="noopener noreferrer">
+        </a>
         {project.title}
         {project.excerpt}
       </div>
--- src/pages/projects.js
+++ src/pages/projects.js
@@ -4,12 +4,12 @@ import ProjectsContainer from '../components/ProjectsComponent/ProjectsContainer
 import styles from '../styles/ProjectsStyles/Projects.module.css';
 
 const projects = [
-    {title: 'Jester: ', excerpt: 'A low code Jest testing suite generator'},
-    {title: 'BookBeats: ', excerpt: 'An application to match your current book with a stylized playlist'},
-    {title: 'WispN: ', excerpt: 'Let us Wisp you away to your next National Park journey!'},
-    {title: 'PTC: ', excerpt: 'Having trouble scheduling those pesky Parent Teacher Conferences?'},
-    {title: 'Snake 3000: ', excerpt: 'An early project to build a retro-styled snake game'},
-    {title: 'Railbird: ', excerpt: 'A Freelanced Dashboard Template for Railbird, a sports contract market'}
+    {title: 'Jester: ', excerpt: 'A low code Jest testing suite generator', link: 'https://jester.software/'},
+    {title: 'BookBeats: ', excerpt: 'An application to match your current book with a stylized playlist', link: 'https://github.com/octo-code-4/bookbeats'},
+    {title: 'WispN: ', excerpt: 'Let us Wisp you away to your next National Park journey!', link: 'https://github.com/wispn-dev/wispn'},
+    {title: 'PTC: ', excerpt: 'Having trouble scheduling those pesky Parent Teacher Conferences?', link: 'https://github.com/Advanced-Education/pt-conf'},
+    {title: 'Snake 3000: ', excerpt: 'An early project to build a retro-styled snake game', link: 'https://jester.software/'},
+    {title: 'Railbird: ', excerpt: 'A Freelanced Dashboard Template for Railbird, a sports contract market', link: 'https://github.com/AnshumanSinha8/Railbird-AS'}
 ]
 const headerContent = {
     title: 'Welcome to my projects!', excerpt: `Take a look around and check out a few projects I'm particularly proud of!`
